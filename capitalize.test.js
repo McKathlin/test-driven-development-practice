00cfadcf77652860f15c1f1a332d4f753cbcd344
McKathlin <mckathlin@gmail.com>
2025-01-11 22:40:11 +0000

    Add more tests and handle empty string case

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,19 @@ import capitalize from './capitalize.js';
 test('capitalizes first letter of single word', () => {
   expect(capitalize('platypus')).toBe('Platypus');
 });
+
+test('capitalizes single letter', () => {
+  expect(capitalize('f')).toBe('F');
+});
+
+test('capitalizes only first word of phrase', () => {
+  expect(capitalize('the quick brown fox')).toBe('The quick brown fox');
+});
+
+test('leaves already-capital first letter unchanged', () => {
+  expect(capitalize('Bob')).toBe('Bob');
+});
+
+test('leaves empty string unchanged', () => {
+  expect(capitalize('')).toBe('');
+});
