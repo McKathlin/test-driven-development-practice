a6de0b69a4935db7e51360e55824d0c4069700c3
McKathlin <mckathlin@gmail.com>
2025-01-13 23:25:56 +0000

    make caesarCipher leave non-letters unchanged

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,9 +1,30 @@
+const LETTER_COUNT = 26;
+
+const FIRST_UPPERCASE_CODE = 'A'.charCodeAt(0);
+const LAST_UPPERCASE_CODE = 'Z'.charCodeAt(0);
+
+const FIRST_LOWERCASE_CODE = 'a'.charCodeAt(0);
+const LAST_LOWERCASE_CODE = 'z'.charCodeAt(0);
+
 export default function caesarCipher(text, shift) {
   let cipherArr = new Array(text.length);
   for (let i = 0; i < text.length; i++) {
-    let charCode = text.charCodeAt(i);
-    let cipherCode = charCode + shift;
-    cipherArr[i] = String.fromCharCode(cipherCode);
+    cipherArr[i] = _encipherChar(text[i], shift);
   }
   return cipherArr.join('');
+}
+
+function _encipherChar(originalChar, shift) {
+  let code = originalChar.charCodeAt(0);
+  let firstCode, lastCode;
+  if (code >= FIRST_UPPERCASE_CODE && code <= LAST_UPPERCASE_CODE) {
+    // It's uppercase.
+    return String.fromCharCode(code + shift);
+  } else if (code >= FIRST_LOWERCASE_CODE && code <= LAST_LOWERCASE_CODE) {
+    // It's lowercase.
+    return String.fromCharCode(code + shift);
+  } else {
+    // It's not a letter. Don't change non-letters.
+    return originalChar;
+  }
 }
