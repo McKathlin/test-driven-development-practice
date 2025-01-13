fa404df4f7d8d68b7e55583569f33ce5602b7855
McKathlin <mckathlin@gmail.com>
2025-01-13 20:54:07 +0000

    Implement calculator subtract, divide, and multiply

--- calculator.js
+++ calculator.js
@@ -2,4 +2,16 @@ function add(a, b) {
   return a + b;
 }
 
-export { add };
+function subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+export { add, subtract, divide, multiply };
