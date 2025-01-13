61528016ae2bff909f5631ac0f45eadbeb2b6556
McKathlin <mckathlin@gmail.com>
2025-01-13 23:05:45 +0000

    Implement caesarCipher() for general case

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,3 +1,9 @@
 export default function caesarCipher(text, shift) {
-  return text; // TODO: implement
+  let cipherArr = new Array(text.length);
+  for (let i = 0; i < text.length; i++) {
+    let charCode = text.charCodeAt(i);
+    let cipherCode = charCode + shift;
+    cipherArr[i] = String.fromCharCode(cipherCode);
+  }
+  return cipherArr.join('');
 }
