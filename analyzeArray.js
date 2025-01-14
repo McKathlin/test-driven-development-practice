3f1395d323abad655c87246a17f484cc0801af17
McKathlin <mckathlin@gmail.com>
2025-01-14 19:54:10 +0000

    Implement all of analyzeArray()

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,10 +1,24 @@
 export default function analyzeArray(arr) {
+  // Empty array edge case
+  if (arr.length == 0) {
+    return { average: null, max: null, min: null, length: 0 };
+  }
+
+  // Start min and max with values that will always be replaced
   let report = {
     average: null,
-    min: null,
-    max: null,
+    min: Infinity,
+    max: -Infinity,
     length: arr.length
   };
-  // TODO: find average, min, and max 
+
+  // Check all elements
+  let sum = 0;
+  for (let element of arr) {
+    sum += element;
+    report.max = Math.max(element, report.max);
+    report.min = Math.min(element, report.min);
+  }
+  report.average = sum / arr.length;
   return report;
 }
