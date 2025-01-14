8a001be87c867af17910988750553fd848c0778b
McKathlin <mckathlin@gmail.com>
2025-01-14 19:44:04 +0000

    Write analyzeArray tests

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,21 +1,41 @@
 import analyzeArray from './analyzeArray.js';
 
 test('Uses nulls when analyzing empty array', () => {
-  // TODO
+  const report = analyzeArray([]);
+  expect(report).toHaveProperty('average', null);
+  expect(report).toHaveProperty('min', null);
+  expect(report).toHaveProperty('max', null);
+  expect(report).toHaveProperty('length', 0);
 });
 
 test('Correctly analyzes single element array', () => {
-  // TODO
+  const report = analyzeArray([32]);
+  expect(report).toHaveProperty('average', 32);
+  expect(report).toHaveProperty('min', 32);
+  expect(report).toHaveProperty('max', 32);
+  expect(report).toHaveProperty('length', 1);
 });
 
 test('Analyzes typical array of positive integers', () => {
-  // TODO
+  const report = analyzeArray([1,8,3,4,2,6]);
+  expect(report).toHaveProperty('average', 4);
+  expect(report).toHaveProperty('min', 1);
+  expect(report).toHaveProperty('max', 8);
+  expect(report).toHaveProperty('length', 6);
 });
 
 test('Correctly handles negative elements', () => {
-  // TODO
+  const report = analyzeArray([-1, 7, -450, 0]);
+  expect(report.average).toBeCloseTo(-111);
+  expect(report).toHaveProperty('min', -450);
+  expect(report).toHaveProperty('max', 7);
+  expect(report).toHaveProperty('length', 4);
 });
 
 test('Correctly handles floating-point elements', () => {
-  // TODO
+  const report = analyzeArray([4.5, 10, -0.1]);
+  expect(report.average).toBeCloseTo(4.8);
+  expect(report).toHaveProperty('min', -0.1);
+  expect(report).toHaveProperty('max', 10);
+  expect(report).toHaveProperty('length', 3);
 });
